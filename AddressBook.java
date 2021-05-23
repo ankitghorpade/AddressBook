1df4f1d035c4e8ec08989b874aa2628c2912796e
unknown <ankitghorpade59@gmail.com>
2021-05-23 07:47:13 -0800

    adding address book

--- AddressBook.java
+++ AddressBook.java
@@ -1,4 +1,4 @@
-  
+   
 package com;
 
 import java.util.LinkedList;
@@ -96,6 +96,15 @@ public class AddressBook {
         }
     }
 
+    //calling method to view contact details by using city name
+    public void viewByCity() {
+        System.out.println("Enter City Name : ");
+        String city = sc.nextLine();
+        for (Contact list : list) {
+            if(list.getCity().equals(city))
+                System.out.println(list);
+        }
+    }
     public static void main(String[] args) {
 
         AddressBook addressBook = new AddressBook();
@@ -103,7 +112,7 @@ public class AddressBook {
         System.out.println("WELCOME TO ADDRESS BOOK PROBLEM");
         //adding multiple persons
         addPersons();
-        //Searching by city
-        searchByCity();
+        //Viewing details by city name
+        addressBook.viewByCity();
     }
 }
