5baa5c1f462e26c7e0f182184b6f3f01da4eeff2
unknown <ankitghorpade59@gmail.com>
2021-05-23 07:26:59 -0800

    adding address book

--- AddressBook.java
+++ AddressBook.java
@@ -9,6 +9,7 @@ public class AddressBook {
     static Scanner sc = new Scanner(System.in);
     static List<Contact> list = new LinkedList<Contact>();
 	private static AddressBook addressBook;
+	private static AddressBook addressBook2;
 
     //Created method for adding contact
     public static void addContact()
@@ -52,21 +53,31 @@ public class AddressBook {
     }
 
 
-    public static void main(String[] args) {
+  //Creating deleteContact() for deleting contact details using first name
+    public static void deleteContact() {
+        System.out.println("Enter first name : ");
+        String firstName = sc.nextLine();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getFirstName().equalsIgnoreCase(firstName)) {
+                list.remove(i);
+            } else {
+                System.out.println("No data found");
+            }
+        }
+}
 
-        AddressBook addressBook = new AddressBook();
-        //Displaying the welcome message
-        System.out.println("WELCOME TO ADDRESS BOOK PROBLEM");
-        //Adding new contact
-        System.out.println("Enter details of new contact");
-        addContact();
-        //Editing Contact
-        editContact();
+public static void main(String[] args) {
 
-
-
-
-
-        System.out.println(list); //printing list
-    }
+    addressBook2 = new AddressBook();
+    //Displaying the welcome message
+    System.out.println("WELCOME TO ADDRESS BOOK PROBLEM");
+    //Adding new contact
+    System.out.println("Enter details of new contact");
+    addContact();
+    //Editing Contact
+    editContact();
+    //deleting contact
+    deleteContact();
+    System.out.println(list); //printing list
+}
 }
